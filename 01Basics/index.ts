c4c7889f7113c25009ee57f390ca7bae36f1e87e
Opilane TTHK <opilane@tthk.ee>
2026-03-18 16:16:03 +0200

    01 basics

---
+++ 01Basics/index.ts
@@ -0,0 +1,28 @@
+//harjutus 1
+type User = {
+    id: string
+    name: string
+    age: number
+    address: {
+        street: string
+        city: string
+    }
+}
+
+//soovin näidata ainut name ja age, aga võetakse kogu objekti sisu, kuna kasutatakse user type
+function renderUserDetails(user: User) {
+    console.log(user.name, user.age)
+}
+
+const user: User {
+    id: "asd",
+    name: "Kyle",
+    age: 23,
+    address: {
+        street: "puukuju",
+        city: string
+    }
+
+}
+
+renderUserDetails(user)
