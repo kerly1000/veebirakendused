e296dbcfc814f0e3105730d52cb288b42ec5c4f6
Opilane TTHK <opilane@tthk.ee>
2026-04-22 13:54:16 +0300

    componence

--- 05ReactWithData/reactwithdata/src/components/ItemForm.tsx
+++ 05ReactWithData/reactwithdata/src/components/ItemForm.tsx
@@ -2,5 +2,14 @@
 
 
 export default function ItemForm({ onAdd}: any) {
-    
+    const [value, setValue] = useState("");
+
+    return (
+        <div>
+            <input value={value} onChange={(e) => setValue(e.target.value)} />
+            <button onClick={() => { onAdd(value); setValue(""); }}>
+                Lisa andmed
+            </button>
+        </div>
+    );
 }
--- 05ReactWithData/reactwithdata/src/components/ItemList.tsx
+++ 05ReactWithData/reactwithdata/src/components/ItemList.tsx
@@ -1,6 +1,15 @@
 
 
 
-export default function ItemList({ onAdd}: any) {
-    
+export default function ItemList({ items, onDelete}: any) {
+    return (
+        <ul>
+            {items.map((i: any) => (
+                <li key={i.id}>
+                    {i.name}
+                    <button onClick={() => onDelete(i.id)}>X</button>
+                </li>
+            ))}
+        </ul>
+    )
 }
